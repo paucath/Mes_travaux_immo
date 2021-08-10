e534f2d236c4b4de80ed53c916053cf709d63964
sbb-ifocop <83959928+sbb-ifocop@users.noreply.github.com>
2021-08-10 18:37:04 +0200

    Page Mentions + maj header/footer

---
+++ MesTravauxImmo/mentionslegales.php
@@ -0,0 +1,22 @@
+    <?php
+        include_once('include/header.html');
+    ?>
+
+        <section class="content">
+            <section class="block">
+                <div class="container">
+                    <h2>Mentions Légales</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas eos hic exercitationem nulla veniam accusantium numquam quidem deserunt distinctio impedit minima rem, culpa vero? Dignissimos laudantium officia qui voluptatibus adipisci!</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas eos hic exercitationem nulla veniam accusantium numquam quidem deserunt distinctio impedit minima rem, culpa vero? Dignissimos laudantium officia qui voluptatibus adipisci!</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas eos hic exercitationem nulla veniam accusantium numquam quidem deserunt distinctio impedit minima rem, culpa vero? Dignissimos laudantium officia qui voluptatibus adipisci!</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas eos hic exercitationem nulla veniam accusantium numquam quidem deserunt distinctio impedit minima rem, culpa vero? Dignissimos laudantium officia qui voluptatibus adipisci!</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas eos hic exercitationem nulla veniam accusantium numquam quidem deserunt distinctio impedit minima rem, culpa vero? Dignissimos laudantium officia qui voluptatibus adipisci!</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas eos hic exercitationem nulla veniam accusantium numquam quidem deserunt distinctio impedit minima rem, culpa vero? Dignissimos laudantium officia qui voluptatibus adipisci!</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas eos hic exercitationem nulla veniam accusantium numquam quidem deserunt distinctio impedit minima rem, culpa vero? Dignissimos laudantium officia qui voluptatibus adipisci!</p>
+                </div>
+            </section>
+        </section>
+
+    <?php
+        include_once('include/footer.html');
+    ?>
