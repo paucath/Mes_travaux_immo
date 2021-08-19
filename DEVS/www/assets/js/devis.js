f767efae872d3b372b96501518919e2b05b49721
paucath <88663722+paucath@users.noreply.github.com>
2021-08-19 15:56:25 +0200

    travail sur front Devis

---
+++ DEVS/www/assets/js/devis.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#content_1").show();
+    $("#content_2a").hide();
+    $("#content_2b").hide();
+    $("#content_3").hide();
+    $("#content_4").hide();
+});
