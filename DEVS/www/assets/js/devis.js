04392f1d9ac6ccc90cc52de8e338c4a18f4573a1
paucath <88663722+paucath@users.noreply.github.com>
2021-08-20 09:39:47 +0200

    fin front devis

--- DEVS/www/assets/js/devis.js
+++ DEVS/www/assets/js/devis.js
@@ -1,3 +1,6 @@
+
+// Document ready
+
 $(document).ready(function() {
     $("#content_1").show();
     $("#content_2a").hide();
@@ -5,3 +8,44 @@ $(document).ready(function() {
     $("#content_3").hide();
     $("#content_4").hide();
 });
+
+function GrosOeuvre(){
+    $("#content_1").hide();
+    $("#content_2a").show();
+    $("#content_2b").hide();
+    $("#content_3").hide();
+    $("#content_4").hide();
+}
+
+function PetitsTrav(){
+    $("#content_1").hide();
+    $("#content_2a").hide();
+    $("#content_2b").show();
+    $("#content_3").hide();
+    $("#content_4").hide();
+}
+
+function valide_type_go() {
+    $("#content_1").hide();
+    $("#content_2a").hide();
+    $("#content_2b").hide();
+    $("#content_3").show();
+    $("#content_4").hide();
+}
+
+function valide_type_pt() {
+    $("#content_1").hide();
+    $("#content_2a").hide();
+    $("#content_2b").hide();
+    $("#content_3").show();
+    $("#content_4").hide();
+}
+
+function valide_infosup(){
+    $("#content_1").hide();
+    $("#content_2a").hide();
+    $("#content_2b").hide();
+    $("#content_3").hide();
+    $("#content_4").show();
+}
+
