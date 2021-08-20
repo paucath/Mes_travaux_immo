7324a8e3b91480848691d34f7040f93dfe67defb
paucath <88663722+paucath@users.noreply.github.com>
2021-08-20 16:16:07 +0200

    fin front tunnel de commande

---
+++ DEVS/www/assets/js/tunnel_commande.js
@@ -0,0 +1,75 @@
+
+// Document ready
+
+$(document).ready(function() {
+    var bulle_1=$("#bulle1");
+    bulle_1.css("background-color" , "#FF0000");
+    $("#Recap_panier").show();
+    $("#connexion").hide();
+    $("#livraison").hide();
+    $("#Paiement").hide();
+    $("#CGV").hide();
+});
+function annule(){
+    var bulle_1=$("#bulle1");
+    bulle_1.css("background-color" , "#FF0000");
+    $("#Recap_panier").show();
+    $("#connexion").hide();
+    $("#livraison").hide();
+    $("#Paiement").hide();
+    $("#CGV").hide();
+}
+function valid_panier(){
+    var bulle_1=$("#bulle1");
+    bulle_1.css("background-color" , "lightgrey");
+    var bulle_2=$("#bulle2");
+    bulle_2.css("background-color" , "#FF0000");
+    $("#Recap_panier").hide();
+    $("#connexion").show();
+    $("#livraison").hide();
+    $("#Paiement").hide();
+    $("#CGV").hide();
+}
+
+function valid_connexion(){
+    var bulle_2=$("#bulle2");
+    bulle_2.css("background-color" , "lightgrey");
+    var bulle_3=$("#bulle3");
+    bulle_3.css("background-color" , "#FF0000");
+    $("#Recap_panier").hide();
+    $("#connexion").hide();
+    $("#livraison").show();
+    $("#Paiement").hide();
+    $("#CGV").hide();
+}
+
+function valid_livraison(){
+    var bulle_3=$("#bulle3");
+    bulle_3.css("background-color" , "lightgrey");
+    var bulle_4=$("#bulle4");
+    bulle_4.css("background-color" , "#FF0000");
+    $("#Recap_panier").hide();
+    $("#connexion").hide();
+    $("#livraison").hide();
+    $("#Paiement").show();
+    $("#CGV").hide();
+}
+
+function valid_payment(){
+    var bulle_4=$("#bulle4");
+    bulle_4.css("background-color" , "lightgrey");
+    var bulle_5=$("#bulle5");
+    bulle_5.css("background-color" , "#FF0000");
+    $("#Recap_panier").hide();
+    $("#connexion").hide();
+    $("#livraison").hide();
+    $("#Paiement").show();
+    $("#CGV").hide();
+    $("#Recap_panier").hide();
+    $("#connexion").hide();
+    $("#livraison").hide();
+    $("#Paiement").hide();
+    $("#CGV").show();
+}
+
+
